598836e32be5f00f54b2b95610a998ad23e5572a
vaishudh <vaishudhamo@gmail.com>
2022-01-27 16:00:05 +0530

    Added config file and include that in dashboard file

--- dashboard.php
+++ dashboard.php
@@ -1,32 +1,7 @@
 <?php
-/* echo $_POST['username']."<br>";
-echo $_POST['email']."<br>";
-echo $_POST['phone']."<br>";
-echo $_POST['age']."<br>";
 
-
-for($i=0;$i < count($_POST['habit']);$i++)
-{
-    echo $_POST['habit'][$i]."<br>";
-}
-
- 
-//$habbit=$_POST["habbit"];,habbit,'$habbit'
-$habit='';
-$habits='';
-
-for($i=0;$i < count($_POST['habit']);$i++)
-{
-    
-$habit .= $_POST['habit'][$i].",";
-
-}
-
-$habits=substr($habit, 0, -1);
-
-echo $_POST['gender']."<br>";
-echo $_POST['password']."<br>"; */
-
+// Create database connection using config file
+include("config.php");
 
 $name=$_POST["username"];
 $email=$_POST["email"];
@@ -44,8 +19,8 @@ $habit .= $_POST['habit'][$i]."";
 }
 
 $password=$_POST["password"];
-$conn = mysqli_connect("localhost", 'root','',"abcbakery");
 
+//Insert Form Data in table
 $query = "INSERT INTO register (name,email,phone,age,gender,password,habbit) VALUES('$name','$email','$phone','$age','$gender','$password','$habit')";
 $result = mysqli_query ($conn,$query);
 
