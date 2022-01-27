d659dbe445ac789c2e0bb58be39560ab8b7728cc
V.Senthilkumar(Leader) <vsenthilk366@gmail.com>
2022-01-27 15:14:53 +0530

    Register Form Worked Successfully

---
+++ contact.php
@@ -0,0 +1,27 @@
+<!Doctype html>
+<html>
+<head>
+<title>ABC Bakery | Contact</title>
+
+<link rel="stylesheet" href="design.css">
+</head>
+<body>
+<nav>
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="contact.php">CONTACT US</a></li>
+    </nav>
+<h1>ABC Bakery</h1>
+<p>ABC Bakery World is an online store that delivers cakes and flowers in 63 locations in Chennai. We make best quality cakes and deliver it on time. 
+We have our own delivery team with strong technological and operations experience which enables us to deliver. </p>
+
+<p>ABC Bakery World is an online store that delivers cakes and flowers in 63 locations in Chennai. We make best quality cakes and deliver it on time. 
+We have our own delivery team with strong technological and operations experience which enables us to deliver. </p>
+
+<p>ABC Bakery World is an online store that delivers cakes and flowers in 63 locations in Chennai. We make best quality cakes and deliver it on time. 
+We have our own delivery team with strong technological and operations experience which enables us to deliver. </p>
+
+
+
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>ABC Bakery | Home</title>
+	
+	
+<link rel="stylesheet" href="design.css">
+</head>
+
+<body>
+    <nav>
+        <li><a href="index.php">HOME</a></li>
+        <li><a href="contact.php">CONTACT US</a></li>
+        <li><a href="register.php">Register</a></li>
+        <li><a href="login.php">Login</a></li>
+    </nav>
+    <h1>ABC Bakery </h1>
+    <p id="para1">ABC Bakery World is an online store that delivers cakes and flowers in 63 locations in Chennai.
+        We make best quality cakes and deliver it on time.
+        We have our own delivery team with strong technological and operations experience which enables us to deliver.
+    </p>
+	<!-- <p id="para2">
+	ABC Bakery World is an online store that delivers cakes and flowers in 63 locations in Chennai.
+        We make best quality cakes and deliver it on time.
+        We have our own delivery team with strong technological and operations experience which enables us to deliver.
+	</p> -->
+	
+	
+	
+
+
+    <h2>Bakery Items</h2>
+
+    <ul>
+        <li>Cakes</li>
+        <li>Icecream</li>
+        <li>Puffs</li>
+    </ul>
+	
+    <img src="Bakery.jpg" alt="Bakery" width="500px" height="300px">
+
+
+<a href="#">About Us</a>
+    <address>
+        First Street,</br>
+        Chennai.
+    </address>
+
+</body>
+
+</html>
